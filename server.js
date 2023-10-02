1062a5a6c1d6c3ed5633f117d462357e74d2c8c6
Salman <Salmanbri7@gmail.com>
2023-10-02 14:16:24 +0300

    the list now works

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+var items = ["task1", "task2", "task 3"];
 
 app.listen(3000);
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("hello");
+  var today = new Date();
+  var day = today.toLocaleDateString("en-UK");
+  res.render("list", { day: day, items: items });
+});
+
+app.post("/", (req, res) => {
+  items.push(req.body.newItem);
+  res.redirect("/");
 });
